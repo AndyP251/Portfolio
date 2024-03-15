dc2e0d7f1cc37ec282fd407c1a26054a66e59899
AndyP251 <andyprince251@gmail.com>
2024-03-15 19:30:27 -0400

    development mode

--- projectWebsite/settings.py
+++ projectWebsite/settings.py
@@ -34,6 +34,9 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
+
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
+
 # Application definition
 
 INSTALLED_APPS = [
