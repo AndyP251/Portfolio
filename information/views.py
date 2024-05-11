63656c11c1183d4e582ff1988f7fc842dd98116f
AndyP251 <andyprince251@gmail.com>
2024-05-11 15:22:42 -0400

    Email Form final changes

--- information/views.py
+++ information/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.conf import settings
 from django.core.mail import send_mail
 from django.shortcuts import reverse
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView, FormView
 from .forms import ContactForm
 
@@ -21,13 +22,14 @@ def getProjects(request):
 class ContactView(FormView):
     form_class = ContactForm
     template_name = "contact.html"
+    success_url = reverse_lazy('success')
 
     def get_success_url(self):
         return reverse("contact")
 
     def form_valid(self, form):
         email = form.cleaned_data.get("email")
-        subject = form.cleaned_data.get("subject")
+        subject = form.cleaned_data.get("Subject")
         message = form.cleaned_data.get("message")
 
         full_message = f"""
@@ -43,6 +45,7 @@ class ContactView(FormView):
             from_email=settings.DEFAULT_FROM_EMAIL,
             recipient_list=[settings.NOTIFY_EMAIL],
         )
+        
         return super(ContactView, self).form_valid(form)
 
 class SuccessView(TemplateView):
