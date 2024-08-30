2385c24d3e350231e9435ad6b5e59db2e0c61bfe
Andrew Prince <aprince@mettel.net>
2024-08-30 17:16:37 -0400

    fixed infinite static load error

--- personalwork/tasks.py
+++ personalwork/tasks.py
@@ -3,13 +3,13 @@ import time
 import threading
 from .views import pull_canvas_data
 
-def schedule_canvas_pull():
-    def run_continuously():
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
+# def schedule_canvas_pull():
+#     def run_continuously():
+#         while True:
+#             schedule.run_pending()
+#             time.sleep(1)
 
-    schedule.every(24).hours.do(pull_canvas_data, canvas_api_url='your_canvas_api_url', access_token='your_access_token')
+#     schedule.every(24).hours.do(pull_canvas_data, canvas_api_url='your_canvas_api_url', access_token='your_access_token')
     
-    continuous_thread = threading.Thread(target=run_continuously)
-    continuous_thread.start()
+#     continuous_thread = threading.Thread(target=run_continuously)
+#     continuous_thread.start()
